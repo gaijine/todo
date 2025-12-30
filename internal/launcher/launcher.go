313dfbb46385e02ae8210d752459e929690bf23c
gaijine <saburoff08@gmail.com>
2025-12-30 20:49:05 +0500

    add function DeletedTask

--- internal/launcher/launcher.go
+++ internal/launcher/launcher.go
@@ -10,32 +10,27 @@ func Menu() {
 	var choice int
 	for {
 		fmt.Println("-------------------------")
-		fmt.Println("1. создать задачу")
-		fmt.Println("2. вывести список задач")
-		fmt.Println("3. пометить выполненной")
-		fmt.Println("4. снять отметку")
-		fmt.Println("0. выйти")
+		fmt.Println("[1] создать задачу")
+		fmt.Println("[2] вывести список задач")
+		fmt.Println("[3] пометить выполненной")
+		fmt.Println("[4] снять отметку")
+		fmt.Println("[5] удалить задачу")
+		fmt.Println("[0] выйти")
 		fmt.Println("-------------------------")
 		fmt.Scanln(&choice)
-
-		var text string
 
 		switch choice {
 
 		case 1:
-			fmt.Println("-------------------------")
-			fmt.Println("введите текст задачи:")
-			fmt.Println("-------------------------")
-			fmt.Scanln(&text)
-			task := tasks.Task{Text: text, Done: false}
-			tasks.List = append(tasks.List, task)
-			log.Printf("[INFO] task created: %s", task.Text)
+			tasks.AddTask()
 		case 2:
 			tasks.ShowTasks()
 		case 3:
 			tasks.MarkDoneTask()
 		case 4:
 			tasks.UnmarkDoneTask()
+		case 5:
+			tasks.DeleteTask()
 		case 0:
 			log.Println("[INFO] application finished")
 			return
--- internal/tasks/task.go
+++ internal/tasks/task.go
@@ -5,6 +5,48 @@ import (
 	"log"
 )
 
+func AddTask() {
+	var text string
+	fmt.Println("-------------------------")
+	fmt.Println("введите текст задачи:")
+	fmt.Println("-------------------------")
+	fmt.Scanln(&text)
+	task := Task{Text: text, Done: false}
+	List = append(List, task)
+	log.Printf("[INFO] task created: %s", task.Text)
+}
+
+func DeleteTask() {
+	if len(List) == 0 {
+		fmt.Println("-------------------------")
+		log.Println("[WARN] task list is empty")
+		fmt.Println("список задач пуст")
+		fmt.Println("-------------------------")
+		return
+	}
+
+	var choice int
+	ShowTasks()
+	fmt.Println("-------------------------")
+	fmt.Println("выберите какую задачу нужно удалить")
+	fmt.Println("-------------------------")
+	fmt.Scanln(&choice)
+
+	if choice >= 1 && choice <= len(List) {
+		deletedTask := List[choice-1]
+		List = append(List[0:choice-1], List[choice:]...)
+		fmt.Println("-------------------------")
+		log.Printf("[INFO] task deleted: id=%d, text=%s", choice, deletedTask.Text)
+		fmt.Println("задача успешно удалена")
+		fmt.Println("-------------------------")
+	} else {
+		fmt.Println("-------------------------")
+		log.Println("[ERROR] invalid number task")
+		fmt.Println("выбран неверный номер")
+		fmt.Println("-------------------------")
+	}
+}
+
 func MarkDoneTask() {
 	if len(List) == 0 {
 		fmt.Println("-------------------------")
